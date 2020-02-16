ab53028e7e44719f0cd03d14f4de7df32316c1ab
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-16 23:30:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+def reverse_each_word(string)
+  words_array = string.split(" ")
+  words_array.collect {|word| word.reverse }.join(" ")
+
+end
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+ 
+
+
